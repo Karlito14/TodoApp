1ada70b1eaf3e42bc00eed8dbf913e77cb230b5e
Karlito14 <leiroz26@hotmail.com>
2024-11-11 10:30:46 +0100

    mise en place du layout

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
-import { Text, SafeAreaView } from 'react-native';
+import { Text, SafeAreaView, View } from 'react-native';
+import { styles } from './App.style';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Hello</Text>
-    </SafeAreaView>
-  )
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <Text>Header</Text>
+        </View>
+        <View style={styles.body}>
+          <Text>Body</Text>
+        </View>
+      </SafeAreaView>
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
+  );
 }
